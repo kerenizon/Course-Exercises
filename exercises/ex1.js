4713bbf7194cb2bc2d21ac72f65302249a911db4
kerenizon <keren.izon@gmail.com>
2020-07-28 16:08:50 +0300

    remove exercises

--- exercises/ex1.js
+++
@@ -1,9 +0,0 @@
-(function () {
-    'use strict';
-
-    const input = require('readline-sync');
-    let name = input.question("What is your name?\n");
-
-    console.log(`Hello ${name}!`);
-
-})()
--- exercises/ex14.js
+++
@@ -1,8 +0,0 @@
-(function () {
-    'use strict';
-
-    let arr = ['a', 3, '!',22, 'abc'];
-    for(let i = 0; i < arr.length; i+= 1){
-        console.log(`${arr[i]}\n`);
-    }
-})()
